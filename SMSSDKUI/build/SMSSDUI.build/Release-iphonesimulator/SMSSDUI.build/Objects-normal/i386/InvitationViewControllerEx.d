bb3b3f06343c27bce30fc23b982cbf702da99e72
David <liyuansheng@liyuanshengdeMac-mini.local>
2016-09-09 10:31:30 +0800

    1、适配iOS10 2、修复已知bug.

--- SMSSDKUI/build/SMSSDUI.build/Release-iphonesimulator/SMSSDUI.build/Objects-normal/i386/InvitationViewControllerEx.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/liyuansheng/Desktop/SMSSDK2.0.5/SMSSDK_2.0.5_Demo/SMSSDKUI/SMSSDKUI/UI/InvitationViewControllerEx.m \
-  /Users/liyuansheng/Desktop/SMSSDK2.0.5/SMSSDK_2.0.5_Demo/SMSSDKUI/SMSSDKUI/UI/InvitationViewControllerEx.h \
-  /Users/liyuansheng/Desktop/SMSSDK2.0.5/SMSSDK_2.0.5_Demo/SMSSDKUI/../SMS_SDK/SMS_SDK.framework/Headers/SMSSDK.h \
-  /Users/liyuansheng/Desktop/SMSSDK2.0.5/SMSSDK_2.0.5_Demo/SMSSDKUI/../SMS_SDK/SMS_SDK.framework/Headers/Extend/SMSSDKResultHanderDef.h \
-  /Users/liyuansheng/Desktop/SMSSDK2.0.5/SMSSDK_2.0.5_Demo/SMSSDKUI/../SMS_SDK/SMS_SDK.framework/Headers/Extend/SMSSDKUserInfo.h \
-  /Users/liyuansheng/Desktop/SMSSDK2.0.5/SMSSDK_2.0.5_Demo/SMSSDKUI/../SMS_SDK/SMS_SDK.framework/Headers/Extend/SMSSDK+DeprecatedMethods.h
